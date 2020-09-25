c9027153f2a05d1154ee0b291c6a6170b7d11437
Natascha <NataschaHof@gmx.de>
2020-09-25 17:19:01 +0200

    useEffect updated

--- src/App.js
+++ src/App.js
@@ -6,36 +6,47 @@ import "./App.css";
 const App = () => {
   const [allPosts, getAllPosts] = useState({});
   const [postUpdate, setPostUpdate] = useState(false);
+  const [updateImage, setUpdateImage] = useState("");
+
+  const apiUrl = "http://localhost:3000/admin/";
+  const newImageUrl = "https://picsum.photos/200/300";
 
   // fetching all posts from API
   useEffect(() => {
-    Axios.get("http://localhost:3000/admin")
-      .then((response) => {
-        getAllPosts(response.data);
-        console.log(response.data);
-        setPostUpdate(false);
-      })
-      .catch((error) => console.error(error));
+    fetchPosts();
   }, [postUpdate]);
 
+  const fetchPosts = async () => {
+    try {
+      const { data } = await Axios.get(apiUrl);
+      getAllPosts(data);
+      console.log(data);
+      setPostUpdate(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   // handle update of specific post in database with a new random image url
-  const editPost = (id) => {
-    Axios.put("http://localhost:3000/admin/" + id, { imageurl: "https://picsum.photos/200/300" })
-      .then((response) => {
-        console.log(response);
-        setPostUpdate(true);
-      })
-      .catch((error) => console.error(error));
+
+  const editPost = async (id) => {
+    setUpdateImage(newImageUrl);
+    try {
+      await Axios.put(apiUrl + id, { imageurl: newImageUrl });
+      setPostUpdate(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   // handle delete of spcific post in database
-  const deletePost = (id) => {
-    Axios.delete("http://localhost:3000/admin/" + id)
-      .then((response) => {
-        console.log(response);
-        setPostUpdate(true);
-      })
-      .catch((error) => console.error(error));
+  const deletePost = async (id) => {
+    try {
+      await Axios.delete(apiUrl + id);
+      setPostUpdate(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   // mapping through data to render travel destination cards and updating state variables via onClick-events of buttons
