98af719f045e9293abb8005c2ccb0db4f09acea8
Natascha <NataschaHof@gmx.de>
2020-09-25 12:04:11 +0200

    delete and edit functionality added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Components/Card";
 import Axios from "axios";
+import "./App.css";
 
 const App = () => {
   const [allPosts, getAllPosts] = useState({});
+  const [postUpdate, setPostUpdate] = useState(false);
 
+  // fetching all posts from API
   useEffect(() => {
     Axios.get("http://localhost:3000/admin")
       .then((response) => {
         getAllPosts(response.data);
         console.log(response.data);
+        setPostUpdate(false);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [postUpdate]);
 
-  /*  const editPost = (id) => {
-    Axios.put("http://localhost:3000/admin/" + { id })
+  // handle update of specific post in database with a new random image url
+  const editPost = (id) => {
+    Axios.put("http://localhost:3000/admin/" + id, { imageurl: "https://picsum.photos/200/300" })
       .then((response) => {
         console.log(response);
-        getAllPosts(response);
+        setPostUpdate(true);
       })
       .catch((error) => console.error(error));
   };
- */
-  const deletePost = (id) => {};
 
+  // handle delete of spcific post in database
+  const deletePost = (id) => {
+    Axios.delete("http://localhost:3000/admin/" + id)
+      .then((response) => {
+        console.log(response);
+        setPostUpdate(true);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  // mapping through data to render travel destination cards and updating state variables via onClick-events of buttons
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      {allPosts && allPosts.length
-        ? allPosts.map((post) => {
-            return (
-              <div>
-                <Card key={post.id} /* handleEdit={editPost} */ handleDelete={deletePost} id={post.id} data={post} />
-              </div>
-            );
-          })
-        : undefined}
+      <h1>Recommended travel destinations</h1>
+      <div className="cardWrapper">
+        {allPosts && allPosts.length
+          ? allPosts.map((post) => {
+              return <Card key={post.id} handleEdit={() => editPost(post.id)} handleDelete={() => deletePost(post.id)} id={post.id} data={post} />;
+            })
+          : undefined}
+      </div>
     </div>
   );
 };
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,17 +4,25 @@ import "../Styles/Card.css";
 
 const Card = (props) => {
   const { handleEdit, handleDelete, id, data } = props;
+
+  let continentClasses = "continent " + data.continent.toLowerCase();
+
   return (
-    <div className="cardWrapper">
-      <h3>Id {id}</h3>
-      <h2>Country: {data.country}</h2>
-      <h2>Location: {data.location}</h2>
-      <h2>Continent: {data.continent}</h2>
-      <h2>Rating: {data.rating}</h2>
+    <div className="card">
+      <h3 className="id">Id: {id}</h3>
+      <h2 className="country">{data.country}</h2>
+      <h2 className="location">{data.location}</h2>
+      <h2 className={continentClasses}>{data.continent}</h2>
+
       <img alt="location" src={data.imageurl} />
       <div className="buttonWrapper">
-        <button onClick={handleEdit}>Edit</button>
-        <button onClick={handleDelete}>Delete</button>
+        <h2 className="rating">Rating: {data.rating}</h2>
+        <button className="button" onClick={handleEdit}>
+          New image
+        </button>
+        <button className="button" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </div>
   );
